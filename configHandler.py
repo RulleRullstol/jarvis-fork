43cbe0f04b9c57bcbee551392f55c23015b4e545
davkj006 <davkj006@student.liu.se>
2025-04-02 21:54:14 +0200

    fixded listenerSocket.py

--- configHandler.py
+++ configHandler.py
@@ -6,90 +6,93 @@ def getConf():
     config.read('./config.ini')
     return config
 
+def getESPCount():
+    conf = getConf()
+    return conf['esp_general']['count']
+
+
 def getLights():
-        '''Returns all light settings as a list of dicts'''
-        lights = []
-        data = []
-        conf = getConf()
-        for sec in conf.sections():             
-                if sec.startswith('light'):
-                        for atr in conf[sec]:
-                                item:dict = {atr: conf[sec][atr]}
-                                data.append(item)
-                        lights.append(data)
-                        data = []        
-        return lights
+    '''Returns all light settings as a list of dicts'''
+    lights = []
+    data = []
+    conf = getConf()
+    for sec in conf.sections():             
+        if sec.startswith('light'):
+            for atr in conf[sec]:
+                item:dict = {atr: conf[sec][atr]}
+                data.append(item)
+            lights.append(data)
+            data = []        
+    return lights
 
 def getSpeakers():
-        '''Returns all speaker settings as a list of dicts'''
-        speakers = []
-        data = []
-        conf = getConf()
-        for sec in conf.sections():
-                if sec.startswith('speaker'):
-                        for atr in conf[sec]:
-                                item:dict = {atr: conf[sec][atr]}
-                                data.append(item)
-                        speakers.append(data)
-                        data = []        
-        return speakers
+    '''Returns all speaker settings as a list of dicts'''
+    speakers = []
+    data = []
+    conf = getConf()
+    for sec in conf.sections():
+        if sec.startswith('speaker'):
+            for atr in conf[sec]:
+                item:dict = {atr: conf[sec][atr]}
+                data.append(item)
+            speakers.append(data)
+            data = []        
+    return speakers
 
 def getValue(secName: str, value: str):
-        '''Return specific value from config as STR'''
-        conf = getConf()
-        return conf[secName].get(value)
+    '''Return specific value from config as STR'''
+    conf = getConf()
+    return conf[secName].get(value)
 
 
 
 def getDictValues(keys: list):
-        values: list = []
-        for key in keys:
-                key = list(key.values())[0]
-                values.append(key)
-        return values
+    values: list = []
+    for key in keys:
+        key = list(key.values())[0]
+        values.append(key)
+    return values
 
 def mergeDicts(list: list):
-        out = {}
-        for d in list:
-                out.update(d)
-        return out
+    out = {}
+    for d in list:
+        out.update(d)
+    return out
 
 def getHAheaders():
-        '''Returns headers for HA POSTs'''
-        conf = getConf()
-        headers = dict(conf['ha_headers'])
-        headers['authorization'] += " " + getValue('home_assistant_settings', 'token')
-        return headers
+    '''Returns headers for HA POSTs'''
+    conf = getConf()
+    headers = dict(conf['ha_headers'])
+    headers['authorization'] += " " + getValue('home_assistant_settings', 'token')
+    return headers
 
 def getLightNames():
-        '''Returns a stringified list of all light aliases'''
-        lightIds = []
-        for light in getLights():
-                lightIds.append(light[0]['alias'])
+    '''Returns a stringified list of all light aliases'''
+    lightIds = []
+    for light in getLights():
+        lightIds.append(light[0]['alias'])
 
-        return str(lightIds)
+    return str(lightIds)
 
 def getManuals():
-        '''Returns all manual settings as a list of dicts'''
-        manuals = []
-        data = []
-        conf = getConf()
-        for sec in conf.sections():             
-                if sec.startswith('manual'):
-                        for atr in conf[sec]:
-                                item:dict = {atr: conf[sec][atr]}
-                                data.append(item)
-                        manuals.append(data)
-                        data = []        
-        return manuals
+    '''Returns all manual settings as a list of dicts'''
+    manuals = []
+    data = []
+    conf = getConf()
+    for sec in conf.sections():             
+        if sec.startswith('manual'):
+            for atr in conf[sec]:
+                item:dict = {atr: conf[sec][atr]}
+                data.append(item)
+            manuals.append(data)
+            data = []        
+    return manuals
 
 def getManualNames():
-        '''Returns a stringified list of all light aliases'''
-        manualIds = []
-        for manual in getManuals():
-                manualIds.append(manual[0]['alias'])
+    '''Returns a stringified list of all light aliases'''
+    manualIds = []
+    for manual in getManuals():
+        manualIds.append(manual[0]['alias'])
 
-        return str(manualIds)
+    return str(manualIds)
 
-
-                       
