d73c0337ced1e2152068146a8fcc821aa55ef077
davkj006 <davkj006@student.liu.se>
2025-04-03 15:57:21 +0200

    fixed listenerSocked

--- listenerSocket.py
+++ listenerSocket.py
@@ -16,23 +16,31 @@ sock.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)  # Allow broadcasting
 # Bind to all interfaces (0.0.0.0) or a specific local IP address
 sock.bind(('0.0.0.0', BROADCAST_PORT))  # Listen on all available interfaces
 
-setup = True
-while setup:
-    data, addr = sock.recvfrom(1024)  # buffer size is 1024 bytes
-    strdata = data.decode('utf-8')
-    
-    if 'Hej hej :)' in strdata:
-        # Get last part of the message to extract the port
-        data = data[-5:]
-        ESP_LIST.append((int(data), addr))
 
-    if len(ESP_LIST) == EXPECTED_ESP:
-        setup = False
-        sock.close()
+def setupESP():
+    """
+    Searches for all expected ESP for first connection.
+    """
+    setup = True
+    while setup:
+        data, addr = sock.recvfrom(1024)
+        strdata = data.decode('utf-8')
+        
+        if 'Hej hej :)' in strdata:
+            data = data[-5:]
+            if not ((int(data), addr) in ESP_LIST):
+                ESP_LIST.append((int(data), addr))
+            print(len(ESP_LIST))
+
+        if len(ESP_LIST) == EXPECTED_ESP:
+            setup = False
+            sock.close()
 
 def get_broadcast_ip():
+    """
+    Fetches general broadcast ip for handshake.
+    """
     hostname = socket.gethostname()
-    local_ip = socket.gethostbyname(hostname)
     
     # Get the subnet mask and broadcast address
     for iface in socket.getaddrinfo(hostname, None):
@@ -49,26 +57,26 @@ def get_broadcast_ip():
     return "255.255.255.255"
 
 def connectToESP(ip: str, port: int):
+    """
+    Connects to one ESP. Done in a seperate thread, once for each ESP
+    to be connected.
+    """
     # Create a new socket in each thread for communication with the ESP
-    sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)  # New UDP socket for each thread
-    sock.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)  # Allow broadcasting
-    #sock.bind(('192.168.1.77', port))  # Bind to a specific port for communication with the ESP
+    sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    sock.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
 
     id = port - 10000  # Calculate ESP ID based on the port
     message = "OK ESP_" + str(id)
     print("1")
-    # Send "OK" message to ESP to acknowledge
-    #sock.sendto(message.encode('utf-8'), (BROADCAST_IP, BROADCAST_PORT))
     sock.sendto(message.encode('utf-8'), (get_broadcast_ip(), BROADCAST_PORT))
 
     print("2")
     loop = True
     while loop:
         print("fuck")
-        data, addr = sock.recvfrom(1024)  # Receive data from ESP
+        data, addr = sock.recvfrom(1024)
         print(f"ESP {id}: {data.decode('utf-8')}")
 
-    # Close the socket when done with communication
     sock.close()
 
 # Start threads for each ESP device
@@ -76,6 +84,10 @@ for esp in ESP_LIST:
     print(f"Connecting to ESP {esp[0]-10000} at {esp[1][0]}")
     thread = threading.Thread(target=connectToESP, args=(esp[1][0], esp[0]), daemon=True)
     thread.start()
+
+
+########################## Start Connection ##########################
+setupESP()
 
 while True:
     pass  # Keep the main thread alive so that threads can run
