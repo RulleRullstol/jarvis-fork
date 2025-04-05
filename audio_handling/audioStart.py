f11f015f5ba7c1395c27d323bf92702be6c22558
RulleRullstol <75090562+RulleRullstol@users.noreply.github.com>
2025-04-05 17:42:59 +0200

    misistt klar och implementerad

--- audio_handling/audioStart.py
+++ audio_handling/audioStart.py
@@ -1,5 +1,7 @@
 import threadSafeList
 import listenerSocket
+import threading
+import MISISTT
 
 class audioStart:
     def __init__(self):
@@ -10,4 +12,5 @@ class audioStart:
         listenerSocket.start(self.pcmList)
         #start STT
         #start MISISTT
-        
+        misisttThread = threading.Thread(target=MISISTT.start, args=(self.pcmList,), daemon=True)
+        misisttThread.start()
