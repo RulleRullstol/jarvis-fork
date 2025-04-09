ab127950f82ff7fdc760bf19bc9eb3aa8dc8b227
RulleRullstol <alwin.svallfors@gmail.com>
2025-04-09 11:54:42 +0200

    configHandler fix

--- src/utils/configHandler.cpp
+++ src/utils/configHandler.cpp
@@ -1,47 +1,44 @@
 #include "configHandler.h"
 #include <iostream>
+#include <string>
 
 using namespace std;
 
-ConfigHandler::ConfigHandler(const string& filename) : reader(filename) {
-    if (reader.ParseError() != 0) {
-        cerr << "Error opening config.ini" << endl;
-    }
-    cout << "Config file loaded successfully." << endl;
+ConfigHandler::ConfigHandler(const string &filename) : reader(filename) {
+  if (reader.ParseError() != 0) {
+    cerr << "Error opening config.ini" << endl;
+  }
+  cout << "Config file loaded successfully." << endl;
 }
 
-vector<string> ConfigHandler::getSections() {
-    return reader.Sections();
+vector<string> ConfigHandler::getSections() { return reader.Sections(); }
+
+vector<string> ConfigHandler::getKeys(const string &section) {
+  return reader.Keys(section);
 }
 
-vector<string> ConfigHandler::getKeys(const string& section) {
-    return reader.Keys(section);
+string ConfigHandler::getValue(const string &section, const string &key) {
+  return reader.Get(section, key, "");
 }
-
-string ConfigHandler::getValue(const string& section, const string& key) {
-    return reader.Get(section, key, "");
-}
-
 
 vector<string> ConfigHandler::getLights() {
-    vector<string> lights;
-    vector<string> sections = getSections();
-    for (const string& sec : sections) {
-        if (sec.find("light") != string::npos) {
-            lights.push_back(sec);
-        }
+  vector<string> lights;
+  vector<string> sections = getSections();
+  for (const string &sec : sections) {
+    if (sec.find("light") != string::npos) {
+      lights.push_back(sec);
     }
-    return lights;
+  }
+  return lights;
 }
 
 // Returns sections containing the str passed
-vector<string> ConfigHandler::findSections(const string& name) {}
-    vector<string> foundSecs;
-    vector<string> sections = getSections();
-    for (const string& secName : sections) {
-        if (secName.contains(name) {
-            foundSecs.push_back(secName);
-        }
-    }
-    return foundSecs;
+vector<string> ConfigHandler::findSections(const string &name) {
+  vector<string> foundSecs;
+  vector<string> sections = ConfigHandler::getSections();
+  for (const string &secName : sections) {
+    if (secName.find(name))
+      foundSecs.push_back(secName);
+  }
+  return foundSecs;
 }
