b9c68f4867dd8f4804a72087474ee81d9b207d9a
RulleRullstol <alwin.svallfors@gmail.com>
2025-04-09 22:42:14 +0200

    webutils med curl

---
+++ src/utils/webUtils.cpp
@@ -0,0 +1,73 @@
+#include <cstddef>
+#include <curl/curl.h>
+#include <stdexcept>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+class WebUtils {
+private:
+  string response;
+  CURLcode res; // Response kod
+  CURL *curl;
+
+  void setHeaders(const vector<string> &headers) {
+    struct curl_slist *chunk = NULL;
+    for (const auto &header : headers)
+      chunk = curl_slist_append(chunk, header.c_str());
+    curl_easy_setopt(curl, CURLOPT_HTTPHEADER, chunk);
+  }
+
+  // Set post/put body
+  void setBody(const string &body) {
+    curl_easy_setopt(curl, CURLOPT_POSTFIELDS, body.c_str());
+    curl_easy_setopt(curl, CURLOPT_POSTFIELDSIZE, body.size());
+  }
+
+  // callback function för att skriva till response. Spännande grunka
+  static size_t writeCallback(void *contents, size_t size, size_t nmemb,
+                              string *userp) { // userp är ptr till reponse
+    size_t totalSize = size * nmemb;
+    string *response = reinterpret_cast<string *>(userp); // userp->reponse
+    response->append((char *)contents, totalSize);
+    return totalSize;
+  }
+
+public:
+  WebUtils() { // init libcurl
+    curl_global_init(CURL_GLOBAL_DEFAULT);
+    curl = curl_easy_init();
+    if (!curl)
+      throw runtime_error("Failed to init libcurl");
+    CURLcode res;
+  }
+
+  // Clean up :)
+  ~WebUtils() {
+    if (curl) {
+      curl_easy_cleanup(curl);
+      curl_global_cleanup();
+    }
+  }
+
+  // Send POST/PUT. returns std::string response
+  string send(const string &url, const vector<string> &headers,
+              const string &body) {
+    if (!curl)
+      throw runtime_error("CURL is not initialized"); // I fall man missat
+    curl_easy_setopt(curl, CURLOPT_URL, url.c_str());
+    setHeaders(headers);
+    setBody(body);
+    curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION,
+                     writeCallback); // Callback för server svar
+    curl_easy_setopt(curl, CURLOPT_WRITEDATA, &response); // Response -> userp
+
+    // Skicka request
+    CURLcode res = curl_easy_perform(curl);
+    if (res != CURLE_OK)
+      throw runtime_error("curl request failed: " +
+                          string(curl_easy_strerror(res)));
+    return response;
+  }
+};
---
+++ src/utils/webUtils.h
@@ -0,0 +1,31 @@
+#ifndef WEBUTILS_H
+#define WEBUTILS_H
+
+#include <cstddef>
+#include <curl/curl.h>
+#include <stdexcept>
+#include <string>
+#include <vector>
+
+class WebUtils {
+private:
+  std::string response;
+  CURLcode res;
+  CURL *curl;
+
+  void setHeaders(const std::vector<std::string> &headers);
+  void setBody(const std::string &body);
+
+  static size_t writeCallback(void *contents, size_t size, size_t nmemb,
+                              std::string *userp);
+
+public:
+  WebUtils();
+  ~WebUtils();
+
+  std::string send(const std::string &url,
+                   const std::vector<std::string> &headers,
+                   const std::string &body);
+};
+
+#endif
