ed7d4374bf1106e27d02990cd75833a3fd8b9538
= <=>
2025-04-11 02:21:49 +0200

    added script for installing dependencies

---
+++ libs/installLibs.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+PACKAGES=(
+  libcurl4-openssl-dev
+  libjsoncpp-dev
+)
+
+echo Installing packages...
+sudo apt update
+
+for pkg in "${PACKAGES[@]}"; do
+  echo "Installing $pkg..."
+  sudo apt install -y $pkg
+done
