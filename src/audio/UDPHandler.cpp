99105ba572c64481881de0d5725be3a0b674cc28
= <=>
2025-04-20 23:28:16 +0200

    UDPHandler constructor ok

--- src/audio/UDPHandler.cpp
+++ src/audio/UDPHandler.cpp
@@ -1,10 +1,9 @@
 #include "audio.h"
 #include <algorithm>
+#include <asio/socket_base.hpp>
 
 UDPHandler::UDPHandler() {
   ConfigHandler conf;
-  broadcastAddr = getBroadcast();
-  broadcastPort = conf.getValue("esp_general", "broadcastPort");
   // Populate esps vector
   int targetEsps = stoi(conf.getValue("esp_general", "count"));
   int maxRetries = targetEsps * stoi(conf.getValue("esp_general", "max_retries"));
@@ -13,17 +12,23 @@ UDPHandler::UDPHandler() {
   string ackTemplate = conf.getValue("esp_general", "ack_msg");
 
   asio::ip::udp::resolver resolver(io_context);
-  // Sending endpoint
-  asio::ip::udp::endpoint broadCastEndpoint = *resolver.resolve(asio::ip::udp::v4(), broadcastAddr, broadcastPort);
   // Recieving endpoint
   asio::ip::udp::endpoint recvEndpoint;
-
+  
+  // Setup socket for recv. Listen to all ports on all interfaces
   asio::ip::udp::socket socket(io_context);
-  array<char, 256> recvBuffer; // 1024 is enough for ack 
+  socket.open(asio::ip::udp::v4());
+  socket.set_option(asio::socket_base::broadcast(true)); // allow broadcast
+  socket.set_option(asio::socket_base::reuse_address(true));
+  socket.bind(asio::ip::udp::endpoint(asio::ip::udp::v4(), 0)); // Bind to all ports on all interfaces
 
+  array<char, 256> recvBuffer; // 1024 is enough for ack 
+  
+  cout << "Listening for ESPs..." << endl;
   while (esps.size() != targetEsps) {
     // Listen for esps
     size_t len = socket.receive_from(asio::buffer(recvBuffer), recvEndpoint);
+    cout << "Packet recieved, parsing..." << endl;
     string recvMsg = string(recvBuffer.data(), len);
   
     // Ta bort msgTemplate och rengöra 
@@ -42,27 +47,39 @@ UDPHandler::UDPHandler() {
       }
       recvMsg = recvMsg.substr(start, end - start);
       
-      // Skapa esp struct
+      // Populera esp struct om vi hittat esp
       int dividerPos = recvMsg.find(" "); // Hitta pos av divider mellan namn och port
       if (dividerPos != string::npos) {
         esp foundESP;
         foundESP.name = recvMsg.substr(1, dividerPos); // Räkna med space mellan ord
         foundESP.endpoint = recvEndpoint;
-        esps.push_back(foundESP);
-        cout << "Found ESP: " << foundESP.name << endl;
+
+        // Stoppa in om inte finns
+        if ([&]() {
+          for (const auto& esp : esps) {
+            if (esp.name == foundESP.name)
+              return true;
+          }
+          return false;
+        }()) {
+          esps.push_back(foundESP);
+        }  
+
+        cout << "Found: " << foundESP.name << " at: " << recvEndpoint.address() << endl;
 
         // Skapa ack msg och skicka till esp
         array<char, 256> ackMsg;
         string tempStr = ackTemplate + " " + foundESP.name;
         copy_n(tempStr.begin(), min(tempStr.size(), ackMsg.size() - 1), ackMsg.begin()); // in med skiten i array
         ackMsg[min(tempStr.size(), ackMsg.size() -1)] = '\0'; // Null termiantor
-        
-
         socket.send_to(asio::buffer(ackMsg.data(), strlen(ackMsg.data())), recvEndpoint);
+        cout << "ACK sent to: " << foundESP.name << " at: " << recvEndpoint.address() << ":" << recvEndpoint.port() << endl;
+        retries++; // Increment retries
       } else {
         cout << "Recieved malformed response from ESP, retrying..." << endl;
       }
     }
+    cout << "Packet not from esp";
 
     if (retries >= maxRetries && esps.size() != 0) {
       cerr << "Max retries reached looking for ESPS on: " << broadcastAddr << ":" << broadcastPort << endl;
